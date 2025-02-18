efefb704e9ee9cc50f7fa0f95be8ecbb417c12b1
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2025-02-18 17:50:17 +0100

    add tests

--- src/aircheck/integrity_check.py
+++ src/aircheck/integrity_check.py
@@ -6,7 +6,6 @@ from aircheck.core.checks import (
     check_dag_id_prefix,
     check_for_duplicated_dags,
     check_for_empty_dag,
-    check_for_whitespace_in_id,
 )
 from aircheck.core.load import load_dags
 from aircheck.core.utils import get_dag_modules
@@ -15,9 +14,7 @@ from aircheck.core.utils import get_dag_modules
 def check_dags_integrity(
     files: list[str],
     dag_path: str,
-    check_prefix: bool,
     dag_id_prefix: str,
-    check_whitespace: bool,
     check_empty_dags: bool,
 ) -> None:
     dags = load_dags(dag_modules=get_dag_modules(dag_path, files))
@@ -27,11 +24,8 @@ def check_dags_integrity(
     for dag in dags:
         check_cycle(dag)
 
-        if check_prefix:
+        if dag_id_prefix:
             check_dag_id_prefix(dag, dag_id_prefix)
-
-        if check_whitespace:
-            check_for_whitespace_in_id(dag)
 
         if check_empty_dags:
             check_for_empty_dag(dag)
--- src/aircheck/main.py
+++ src/aircheck/main.py
@@ -9,30 +9,25 @@ DEFAULT_DAG_PATH = str(pathlib.Path.cwd() / "dags")
 
 @click.command()
 @click.argument("files", nargs=-1, type=click.Path(exists=True))
-@click.option("--dag_path", type=click.Path(exists=True), default=DEFAULT_DAG_PATH)
 @click.option(
-    "--check-prefix", is_flag=True, help="Check if DAG ID starts with a given prefix."
+    "--dag-path",
+    type=click.Path(exists=True),
+    default=DEFAULT_DAG_PATH,
+    help="Path where DAG files are",
 )
-@click.option("--dag-id-prefix", default="", help="DAG ID prefix to check against.")
-@click.option(
-    "--check-whitespace", is_flag=True, help="Check for whitespace in DAG ID."
-)
+@click.option("--dag-id-prefix", default="", help="DAG ID prefix to enforce.")
 @click.option("--check-empty-dags", is_flag=True, help="Check for empty DAGs.")
 def main(
     files: list[str],
     dag_path: str,
-    check_prefix: bool,
     dag_id_prefix: str,
-    check_whitespace: bool,
     check_empty_dags: bool,
 ) -> None:
     """CLI entry point for DAG integrity validation."""
     check_dags_integrity(
         files=files,
         dag_path=dag_path,
-        check_prefix=check_prefix,
         dag_id_prefix=dag_id_prefix,
-        check_whitespace=check_whitespace,
         check_empty_dags=check_empty_dags,
     )
 
